1ab612a27fa2ef51cedbde0ecfc766dfbfde9357
Devanarayan J <devan23.5.2001@gmail.com>
2022-10-17 23:40:34 +0530

    Fix bug

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Input() {
     return (
-        <div className="text-white w-full h-full font-mono flex flex-col space-y-2">
+        <div className="text-white w-full h-full font-mono flex">
             <p className="text-green-600 text-start">terminal_to_cloud:~$</p>
             <input className='bg-black ml-2 border-0 focus:outline-none flex-grow' />
         </div>
