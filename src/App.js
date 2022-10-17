244cb8d53896a2a295565c3dd1adf1ed28b0ee47
gokz1119 <gokz1119@gmail.com>
2022-10-17 16:03:55 +0530

    Added an Input component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
+import Input from './Components/Input.jsx';
 
 function App() {
   return (
     <div className="App">
-      <div className="text-white w-full h-full">
-        <p className="text-green-600 text-start">terminal_to_cloud:~$ <span>_</span>
-        <input></input>
-        </p>
-      </div>
+      <Input />
     </div>
   );
 }
